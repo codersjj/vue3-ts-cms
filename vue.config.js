fe9842c8f487d414243a95d22f9b2a35d914b1a2
Shane678 <shanewestlife@outlook.com>
2022-01-25 21:54:05 +0800

    build: 更新 vue.config.js 配置文件（方式二：使用 configureWebpack 函数添加配置）

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
+const path = require('path')
+
 module.exports = {
   // 配置方式一：使用 Vue CLI 提供的属性
   outputDir: './build',
-  // 配置方式二：使用 configureWebpack 对象（和 webpack 中的属性完全一致，最后会进行合并）
-  configureWebpack: {
-    resolve: {
-      alias: {
-        components: '@/components'
-      }
+  // 配置方式二：使用 configureWebpack 函数（和 webpack 中的属性完全一致，最后会进行合并）
+  configureWebpack: (config) => {
+    config.resolve.alias = {
+      '@': path.resolve(__dirname, 'src'),
+      components: '@/components'
     }
   }
 }
