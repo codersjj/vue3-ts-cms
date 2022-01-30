03d21c8ae93bbeea397f230739eeeccb79ab24c5
Shane678 <shanewestlife@outlook.com>
2022-01-30 21:43:28 +0800

    perf: （按需）导入 Element Plus_v2（只导入使用到的组件的样式）
    项目的集成之 element-plus 的集成

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,8 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { ElButton } from 'element-plus'
+import 'element-plus/theme-chalk/base.css'
+import 'element-plus/theme-chalk/el-button.css'
 
 export default defineComponent({
   name: 'App',
