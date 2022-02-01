1a2174e91e3d8b2d11f91712dff60a9d31f74586
Shane678 <shanewestlife@outlook.com>
2022-02-01 14:59:32 +0800

    perf: （按需）导入 Element Plus_v3（Vue CLI 配置 Webpack 插件实现自动导入使用到的组件的样式）
    项目的集成之 element-plus 的集成

--- src/App.vue
+++ src/App.vue
@@ -19,8 +19,6 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { ElButton } from 'element-plus'
-import 'element-plus/theme-chalk/base.css'
-import 'element-plus/theme-chalk/el-button.css'
 
 export default defineComponent({
   name: 'App',
