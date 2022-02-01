4927c87d7b0904fbe6415e7d29e7b973373586f7
Shane678 <shanewestlife@outlook.com>
2022-02-01 16:32:27 +0800

    refactor: （按需）导入 Element Plus_v7（封装全局注册多个组件的功能）
    项目的集成之 element-plus 的集成

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,5 @@
 import { createApp } from 'vue'
-
-import {
-  ElButton,
-  ElAlert,
-  ElAside,
-  ElForm,
-  ElFormItem,
-  ElInput,
-  ElRadio
-} from 'element-plus'
-
-const components = [
-  ElButton,
-  ElAlert,
-  ElAside,
-  ElForm,
-  ElFormItem,
-  ElInput,
-  ElRadio
-]
+import { registerApp } from './global'
 
 import App from './App.vue'
 
@@ -26,12 +7,7 @@ import router from './router'
 import store from './store'
 
 const app = createApp(App)
-
-// 全局注册 ElButton 等组件
-for (const component of components) {
-  app.component(component.name, component)
-}
-
+registerApp(app)
 app.use(router)
 app.use(store)
 app.mount('#app')
