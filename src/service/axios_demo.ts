2063dfb4448dcffd667311c9e72b4b4aedb40f77
Shane678 <shanewestlife@outlook.com>
2022-02-03 21:58:34 +0800

    feat: axios.all()：多个请求，一起返回
    项目的集成之 axios 的集成

--- src/service/axios_demo.ts
+++ src/service/axios_demo.ts
@@ -8,31 +8,14 @@ axios.get('http://123.207.32.32:8000/home/multidata').then((res) => {
 // axios 的常用（全局）配置选项（全局意味着后续每个请求都会有的配置）
 axios.defaults.baseURL = 'http://httpbin.org'
 axios.defaults.timeout = 10000
-// axios.defaults.headers = {}
 
-// get 请求（带请求参数）
+// axios.all()：多个请求，一起返回
 axios
-  .get('/get', {
-    params: {
-      name: 'coderzhj',
-      age: 20
-    },
-    // 只针对当前请求的配置
-    timeout: 5000,
-    headers: {}
-  })
+  .all([
+    axios.get('/get', { params: { name: 'coderzhj', age: 20 } }),
+    axios.post('/post', { data: { name: 'zhj', age: 20 } })
+  ])
   .then((res) => {
-    console.log(res.data)
-  })
-
-// post 请求
-axios
-  .post('/post', {
-    data: {
-      name: 'zhj',
-      age: 20
-    }
-  })
-  .then((res) => {
-    console.log(res.data)
+    console.log(res[0].data)
+    console.log(res[1].data)
   })
