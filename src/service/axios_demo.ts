53a0b01af4b3fd101b97f77ff74387e0a95e7b14
Shane678 <shanewestlife@outlook.com>
2022-02-03 22:35:32 +0800

    feat: axios 的拦截器添加示例
    项目的集成之 axios 的集成

--- src/service/axios_demo.ts
+++ src/service/axios_demo.ts
@@ -19,3 +19,34 @@ axios
     console.log(res[0].data)
     console.log(res[1].data)
   })
+
+// axios 的拦截器
+// fn1: 请求发送成功会执行的函数
+// fn2: 请求发送失败会执行的函数
+// axios.interceptors.request.use(fn1, fn2)
+axios.interceptors.request.use(
+  (config) => {
+    // 想做的一些操作
+    // 比如：给请求添加 token
+    // 比如：isLoading 动画
+    console.log('请求成功的拦截')
+    return config
+  },
+  (err) => {
+    console.log('请求发送错误')
+    return err
+  }
+)
+// fn1: 数据响应成功（服务器正常地返回了数据，状态码一般为 20x）会执行的函数
+// fn2: 数据响应失败（服务器返回数据失败，状态码一般为 40x/50x）会执行的函数
+// axios.interceptors.response.use(fn1, fn2)
+axios.interceptors.response.use(
+  (res) => {
+    console.log('响应成功的拦截')
+    return res
+  },
+  (err) => {
+    console.log('服务器响应失败')
+    return err
+  }
+)
