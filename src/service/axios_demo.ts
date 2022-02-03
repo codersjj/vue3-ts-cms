2fc440164d5de201c5694c5b56bfe5b697fa472c
Shane678 <shanewestlife@outlook.com>
2022-02-03 21:49:12 +0800

    feat: axios 常用配置选项的配置示例
    项目的集成之 axios 的集成

--- src/service/axios_demo.ts
+++ src/service/axios_demo.ts
@@ -5,13 +5,21 @@ axios.get('http://123.207.32.32:8000/home/multidata').then((res) => {
   console.log(res.data)
 })
 
+// axios 的常用（全局）配置选项（全局意味着后续每个请求都会有的配置）
+axios.defaults.baseURL = 'http://httpbin.org'
+axios.defaults.timeout = 10000
+// axios.defaults.headers = {}
+
 // get 请求（带请求参数）
 axios
-  .get('http://httpbin.org/get', {
+  .get('/get', {
     params: {
       name: 'coderzhj',
       age: 20
-    }
+    },
+    // 只针对当前请求的配置
+    timeout: 5000,
+    headers: {}
   })
   .then((res) => {
     console.log(res.data)
@@ -19,7 +27,7 @@ axios
 
 // post 请求
 axios
-  .post('http://httpbin.org/post', {
+  .post('/post', {
     data: {
       name: 'zhj',
       age: 20
