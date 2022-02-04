6fd386946e04a74ed2c160934a7295276b5f6d79
Shane678 <shanewestlife@outlook.com>
2022-02-04 14:24:43 +0800

    build: 将 npm run build 命令构建出来的 index.html 文件中的资源路径默认从根目录开始修改为相对路径

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,8 @@ const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 module.exports = {
   // 配置方式一：使用 Vue CLI 提供的属性
   outputDir: './build',
+  // 将所有的资源都链接为相对路径（但部署到服务器上时通常不会这样设置）
+  publicPath: './',
   // 配置方式三：使用 chainWebpack 函数（链式方式配置）
   chainWebpack: (config) => {
     config.resolve.alias
