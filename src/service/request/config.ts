ec6f791d43e8f731b3e6f9d8881c62c3e14a9114
Shane678 <shanewestlife@outlook.com>
2022-02-04 13:32:43 +0800

    feat: 项目不同环境变量的区分（方式一：手动地切换不同的环境（不推荐））

---
+++ src/service/request/config.ts
@@ -0,0 +1,9 @@
+// 1. 方式一：手动地切换不同的环境（不推荐）
+// const BASE_URL = 'http://coderzhj.org/dev'
+// const VUE_APP_NAME = 'coderzhj'
+
+// const BASE_URL = 'http://coderzhj.org/prod'
+// const VUE_APP_NAME = 'coderwy'
+
+const BASE_URL = 'http://coderzhj.org/test'
+const VUE_APP_NAME = 'coderzjq'
