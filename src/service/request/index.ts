69770cf11ff3afe07c74c98f34886573f01f1669
Shane678 <shanewestlife@outlook.com>
2022-02-07 23:19:03 +0800

    feat: jJRequest 全局拦截器（所有 axios 实例都有的拦截器）的封装
    项目的集成之 axios 的集成

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -8,8 +8,9 @@ class JJRequest {
 
   constructor(config: JJRequestConfig) {
     this.instance = axios.create(config)
+    // 从 config 中取出的拦截器是对应的 axios 实例的拦截器
     this.interceptors = config.interceptors
-
+    // 添加对应的 axios 实例所有的拦截器
     this.instance.interceptors.request.use(
       this.interceptors?.requestInterceptor,
       this.interceptors?.requestInterceptorCatch
@@ -17,6 +18,28 @@ class JJRequest {
     this.instance.interceptors.response.use(
       this.interceptors?.responseInterceptor,
       this.interceptors?.responseInterceptorCatch
+    )
+
+    // 添加所有 axios 实例都有的拦截器
+    this.instance.interceptors.request.use(
+      (config) => {
+        console.log('所有 axios 实例都有的拦截器：请求成功的拦截')
+        return config
+      },
+      (err) => {
+        console.log('所有 axios 实例都有的拦截器：请求失败的拦截')
+        return err
+      }
+    )
+    this.instance.interceptors.response.use(
+      (res) => {
+        console.log('所有 axios 实例都有的拦截器：响应成功的拦截')
+        return res
+      },
+      (err) => {
+        console.log('所有 axios 实例都有的拦截器：响应失败的拦截')
+        return err
+      }
     )
   }
   request(config: AxiosRequestConfig): void {
