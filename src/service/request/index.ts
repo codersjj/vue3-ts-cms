60f974560d4b6dc4efa9caf7a1d99bee5f008dfb
Shane678 <shanewestlife@outlook.com>
2022-02-07 22:00:18 +0800

    feat: jJRequest 的请求 config 拦截器的封装
    项目的集成之 axios 的集成

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -1,9 +1,33 @@
 import axios from 'axios'
-import type { AxiosInstance, AxiosRequestConfig } from 'axios'
+import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
+
+interface JJRequestInterceptors {
+  requestInterceptor?: (config: AxiosRequestConfig) => AxiosRequestConfig
+  requestInterceptorCatch?: (error: any) => any
+  responseInterceptor?: (res: AxiosResponse) => AxiosResponse
+  responseInterceptorCatch?: (error: any) => any
+}
+
+interface JJRequestConfig extends AxiosRequestConfig {
+  interceptors?: JJRequestInterceptors
+}
+
 class JJRequest {
   instance: AxiosInstance
-  constructor(config: AxiosRequestConfig) {
+  interceptors?: JJRequestInterceptors
+
+  constructor(config: JJRequestConfig) {
     this.instance = axios.create(config)
+    this.interceptors = config.interceptors
+
+    this.instance.interceptors.request.use(
+      this.interceptors?.requestInterceptor,
+      this.interceptors?.requestInterceptorCatch
+    )
+    this.instance.interceptors.response.use(
+      this.interceptors?.responseInterceptor,
+      this.interceptors?.responseInterceptorCatch
+    )
   }
   request(config: AxiosRequestConfig): void {
     this.instance.request(config).then((res) => {
