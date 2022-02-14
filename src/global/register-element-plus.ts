bfe6f10f1003b66f6264cf6eacf8a1e34afd7f8a
Shane678 <shanewestlife@outlook.com>
2022-02-14 21:16:28 +0800

    feat(main): 首页布局的搭建
    首页 - 首页布局的搭建

--- src/global/register-element-plus.ts
+++ src/global/register-element-plus.ts
@@ -1,25 +1,33 @@
 import { App } from 'vue'
 // 如果没有安装 unplugin-vue-components 和 unplugin-auto-import 这两款插件开启 element-plus 的按需自动导入功能，那么需要在这里按需手动导入使用到的 element-plus 组件
 import {
+  ElAside,
   ElButton,
   ElCheckbox,
+  ElContainer,
   ElForm,
   ElFormItem,
+  ElHeader,
   ElIcon,
   ElInput,
   ElLink,
+  ElMain,
   ElTabPane,
   ElTabs
 } from 'element-plus'
 
 const components = [
+  ElAside,
   ElButton,
   ElCheckbox,
+  ElContainer,
   ElForm,
   ElFormItem,
+  ElHeader,
   ElIcon,
   ElInput,
   ElLink,
+  ElMain,
   ElTabPane,
   ElTabs
 ]
--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Main</div>
+  <div class="main">
+    <el-container class="main-content">
+      <el-aside width="210px">Aside</el-aside>
+      <el-container class="page">
+        <el-header class="page-header">Header</el-header>
+        <el-main class="page-content">Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script lang="ts">
@@ -12,4 +20,48 @@ export default defineComponent({
 })
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.main {
+  // position: fixed;
+  // top: 0;
+  // left: 0;
+  // width: 100%;
+  height: 100%;
+}
+
+.main-content,
+.page {
+  height: 100%;
+}
+
+.main-content {
+  display: flex;
+
+  .el-aside {
+    background: #001529;
+  }
+
+  .page-header {
+    background: #f0f2f5;
+  }
+
+  .page-content {
+    height: calc(100% - 48px);
+    background: #888;
+  }
+}
+
+.el-header {
+  display: flex;
+  text-align: center;
+  align-items: center;
+  color: #333;
+
+  height: 48px !important;
+}
+
+.el-main {
+  text-align: center;
+  color: #333;
+}
+</style>
