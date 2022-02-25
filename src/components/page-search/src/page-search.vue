a8c6f8f7b70b8e040cd521f674c56ba45e52e452
Shane678 <shanewestlife@outlook.com>
2022-02-25 23:04:16 +0800

    feat: 页面头部搜索区域 PageSearch 的封装

---
+++ src/components/page-search/src/page-search.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="page-search">
+    <jj-form v-bind="searchFormConfig" v-model="formData">
+      <template v-slot:header>
+        <h2 class="header">高级检索</h2>
+      </template>
+      <template #footer>
+        <div class="handle-btns">
+          <el-button>
+            <el-icon><refresh-right /></el-icon>
+            <span>重置</span>
+          </el-button>
+          <el-button type="primary">
+            <el-icon><search /></el-icon>
+            <span>搜索</span>
+          </el-button>
+        </div>
+      </template>
+    </jj-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import JjForm from '@/base-ui/form'
+
+export default defineComponent({
+  props: {
+    searchFormConfig: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    JjForm
+  },
+  setup() {
+    // 这里使用了 ref，而没有使用 reactive 的原因是：在使用 v-model 进行（元素/组件的）双向绑定时 reactive 会有些问题
+    // 暂且写死 formData 的内容，后期再动态传入
+    const formData = ref({
+      id: '',
+      name: '',
+      password: '',
+      sport: '',
+      createTime: ''
+    })
+    return { formData }
+  }
+})
+</script>
+
+<style scoped>
+.header {
+  color: red;
+  /* text-align: left; */
+}
+
+.handle-btns {
+  text-align: right;
+  padding: 0 50px 20px 0;
+}
+</style>
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,58 +1,23 @@
 <template>
   <div class="user">
-    <jj-form v-bind="searchFormConfig" v-model="formData">
-      <template v-slot:header>
-        <h2 class="header">高级检索</h2>
-      </template>
-      <template #footer>
-        <div class="handle-btns">
-          <el-button>
-            <el-icon><refresh-right /></el-icon>
-            <span>重置</span>
-          </el-button>
-          <el-button type="primary">
-            <el-icon><search /></el-icon>
-            <span>搜索</span>
-          </el-button>
-        </div>
-      </template>
-    </jj-form>
+    <page-search :searchFormConfig="searchFormConfig" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
-import JjForm from '@/base-ui/form'
+import { defineComponent } from 'vue'
+import PageSearch from '@/components/page-search'
 import { searchFormConfig } from './config/search.config'
 
 export default defineComponent({
   name: 'user',
   components: {
-    JjForm
+    PageSearch
   },
   setup() {
-    // 这里使用了 ref，而没有使用 reactive 的原因是：在使用 v-model 进行（元素/组件的）双向绑定时 reactive 会有些问题
-    const formData = ref({
-      id: '',
-      name: '',
-      password: '',
-      sport: '',
-      createTime: ''
-    })
-
-    return { searchFormConfig, formData }
+    return { searchFormConfig }
   }
 })
 </script>
 
-<style scoped>
-.header {
-  color: red;
-  /* text-align: left; */
-}
-
-.handle-btns {
-  text-align: right;
-  padding: 0 50px 20px 0;
-}
-</style>
+<style scoped></style>
