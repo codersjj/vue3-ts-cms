2823909d497ec0682ca55321cf36ab32fefe75e1
Shane678 <shanewestlife@outlook.com>
2022-02-26 18:10:35 +0800

    feat(table): jjTable 中的 column 中使用作用域插槽，实现数据的个性化展示

--- src/base-ui/table/src/table.vue
+++ src/base-ui/table/src/table.vue
@@ -3,14 +3,25 @@
     <el-table :data="tableData" border style="width: 100%">
       <template v-for="attributes in attributesList" :key="attributes.prop">
         <!-- <el-table-column prop="name" label="姓名" min-width="180" /> -->
-        <el-table-column v-bind="attributes" align="center" />
+        <el-table-column v-bind="attributes" align="center">
+          <!-- el-table-column 中是提供了插槽的 -->
+          <!-- <el-button>666</el-button> -->
+          <!-- 即 -->
+          <!-- <template #default>
+            <el-button>666</el-button>
+          </template> -->
+          <!-- 使用作用域插槽，实现数据的个性化展示 -->
+          <template #default="slotProps">
+            <el-button>{{ slotProps.row[attributes.prop] }}</el-button>
+          </template>
+        </el-table-column>
       </template>
     </el-table>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from 'vue'
 
 export default defineComponent({
   props: {
@@ -19,7 +30,7 @@ export default defineComponent({
       required: true
     },
     attributesList: {
-      type: Array,
+      type: Array as PropType<any[]>,
       required: true
     }
   },
