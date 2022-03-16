630f3a417c99aca68ed25b17f5c47cb62ee661e8
Shane678 <shanewestlife@outlook.com>
2022-03-16 20:53:37 +0800

    feat(role): 角色管理添加角色编辑页面

---
+++ src/views/main/system/role/config/modal.config.ts
@@ -0,0 +1,20 @@
+import { IForm } from '@/base-ui/form'
+
+export const modalFormConfig: IForm = {
+  formItems: [
+    {
+      field: 'name',
+      type: 'input',
+      label: '角色名称',
+      placeholder: '请输入角色名称'
+    },
+    {
+      field: 'intro',
+      type: 'input',
+      label: '角色介绍',
+      placeholder: '请输入角色介绍'
+    }
+  ],
+  colLayout: { span: 24 },
+  itemStyle: { padding: '8px 30px 8px 10px' }
+}
--- src/views/main/system/role/role.vue
+++ src/views/main/system/role/role.vue
@@ -10,11 +10,21 @@
       :contentTableConfig="contentTableConfig"
       parentName="system"
       pageName="role"
+      @editBtnClick="handleEditBtnClick"
     >
       <template #create>
-        <el-button type="primary">新建角色</el-button>
+        <el-button type="primary" @click="handleNewBtnClick"
+          >新建角色</el-button
+        >
       </template>
     </page-content>
+    <page-modal
+      ref="pageModalRef"
+      :modalFormConfig="modalFormConfig"
+      pageName="role"
+      :defaultInfo="defaultInfo"
+      :title="Object.keys(defaultInfo).length ? '编辑角色' : '新建角色'"
+    />
   </div>
 </template>
 
@@ -23,28 +33,40 @@ import { defineComponent } from 'vue'
 
 import PageSearch from '@/components/page-search'
 import PageContent from '@/components/page-content'
+import PageModal from '@/components/page-modal'
 
 import { searchFormConfig } from './config/search.config'
 import { contentTableConfig } from './config/content.config'
+import { modalFormConfig } from './config/modal.config'
 
 import { usePageSearch } from '@/hooks/usePageSearch'
+import { usePageModal } from '@/hooks/usePageModal'
 
 export default defineComponent({
   name: 'role',
   components: {
     PageSearch,
-    PageContent
+    PageContent,
+    PageModal
   },
   setup() {
     const [pageContentRef, handleResetBtnClick, handleQueryBtnClick] =
       usePageSearch()
 
+    const [pageModalRef, defaultInfo, handleNewBtnClick, handleEditBtnClick] =
+      usePageModal()
+
     return {
       searchFormConfig,
       contentTableConfig,
+      modalFormConfig,
       pageContentRef,
+      pageModalRef,
+      defaultInfo,
       handleResetBtnClick,
-      handleQueryBtnClick
+      handleQueryBtnClick,
+      handleNewBtnClick,
+      handleEditBtnClick
     }
   }
 })
