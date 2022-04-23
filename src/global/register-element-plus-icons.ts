e76ef6226406fe4b234244453fd62c79291db84b
Shane678 <shanewestlife@outlook.com>
2022-04-23 10:31:07 +0800

    perf: 取消引入所有的 icon，现在自己按需引入 icon

--- src/global/register-element-plus-icons.ts
+++ src/global/register-element-plus-icons.ts
@@ -3,12 +3,53 @@ import { App } from 'vue'
 // CommonJS 方式引入
 // const elementPlusIconsObj = require('@element-plus/icons-vue')
 // ESM 方式引入
-import * as elementPlusIconsObj from '@element-plus/icons-vue'
+// import * as elementPlusIconsObj from '@element-plus/icons-vue'
+
+// export default function (app: App) {
+//   // 对每个 icon 都进行全局注册
+//   for (const key in elementPlusIconsObj) {
+//     const icon = elementPlusIconsObj[key as keyof typeof elementPlusIconsObj]
+//     app.component(icon.name, icon)
+//   }
+// }
+
+// 按需引入
+import {
+  Cellphone,
+  ChatLineRound,
+  CircleClose,
+  Delete,
+  Edit,
+  Expand,
+  Fold,
+  Goods,
+  Monitor,
+  Refresh,
+  RefreshRight,
+  Search,
+  Setting,
+  User
+} from '@element-plus/icons-vue'
+
+const icons = [
+  Cellphone,
+  ChatLineRound,
+  CircleClose,
+  Delete,
+  Edit,
+  Expand,
+  Fold,
+  Goods,
+  Monitor,
+  Refresh,
+  RefreshRight,
+  Search,
+  Setting,
+  User
+]
 
 export default function (app: App) {
-  // 对每个 icon 都进行全局注册
-  for (const key in elementPlusIconsObj) {
-    const icon = elementPlusIconsObj[key as keyof typeof elementPlusIconsObj]
+  for (const icon of icons) {
     app.component(icon.name, icon)
   }
 }
